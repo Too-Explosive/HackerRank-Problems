ac412ba76ec1eb4d8bb82d340d5f5e394f94a569
Too-Explosive <mukurkur@gmail.com>
2017-05-18 09:47:40 -0700

    Add files via upload

---
+++ Python/Python If-Else.py
@@ -0,0 +1,10 @@
+import sys
+n = int(input().strip())
+if n % 2 != 0:
+    print("Weird")
+elif n >= 2 and n <= 5:
+    print("Not Weird")
+elif n > 5 and n < 21:
+     print("Weird")
+else:
+     print("Not Weird")
