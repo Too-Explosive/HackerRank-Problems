a5203fa033013d2ae0ab5d6246c65a9ba9a6440b
Too-Explosive <mukurkur@gmail.com>
2017-05-19 09:46:06 -0700

    Add files via upload

---
+++ hey.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    vector<int> a(x);
+    for (int i = 0; i < x; i++)
+        cin >> a[i];
+    for (int i = x - 1; i >= 0; i--)
+        cout << a[i] << " ";
+    return 0;
+}
