e11ab8d70440c559eef959d34ee688fdb0791cd0
Too-Explosive <mukurkur@gmail.com>
2017-05-19 09:51:16 -0700

    Add files via upload

---
+++ Cplusplus/Introduction/hey.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    long x;
+    long long y;
+    char z;
+    float b;
+    long double a;
+    cin >> n >> x >> y >> z >> b >> a;
+    cout << setprecision(20) << n << endl << x << endl << y << endl << z << endl << b << endl << a;
+    return 0;
+}
