bc183752005d763fbfe4344f7a6dc094ca3ca507
Too-Explosive <mukurkur@gmail.com>
2017-05-25 08:48:09 -0700

    Create Day 1: Data Types.cpp

---
+++ 30 Days of Code/Day 1: Data Types.cpp
@@ -0,0 +1,7 @@
+    int x;
+    double a;
+    string sh;
+    cin >> x >> a;
+    cin.ignore();
+    getline(cin, sh);
+    cout << x + i << endl << fixed << setprecision(1) << d + a << endl << s + sh;
