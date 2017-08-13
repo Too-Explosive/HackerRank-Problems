574a55e69b7a25abff914af688527bd4e55fd931
Too-Explosive <mukurkur@gmail.com>
2017-08-12 17:24:11 -0700

    Create Array - Addition.rb

---
+++ Ruby/Arrays and Hashes/Array - Addition.rb
@@ -0,0 +1,15 @@
+def end_arr_add(arr, element)
+    arr.push(element)
+    return arr
+end
+def begin_arr_add(arr, element)
+    arr.unshift(element)
+    return arr
+end
+def index_arr_add(arr, index, element)
+    arr.insert(index, element)
+    return arr
+end
+def index_arr_multiple_add(arr, index)
+    arr.insert(index, 10, 2)
+end
