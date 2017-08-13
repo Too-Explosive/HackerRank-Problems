9ae62ba3ccd360053d2ec98b6a5b2e8b53d8b005
Too-Explosive <mukurkur@gmail.com>
2017-08-12 17:32:16 -0700

    Create Array - Selection.rb

---
+++ Ruby/Arrays and Hashes/Array - Selection.rb
@@ -0,0 +1,12 @@
+def select_arr(arr)
+  return arr.select {|a| a % 2 != 0}
+end
+def reject_arr(arr)
+  return arr.reject {|a| a % 3 == 0}
+end
+def delete_arr(arr)
+  arr.delete_if {|a| a < 0}
+end
+def keep_arr(arr)
+  arr.keep_if {|a| a >= 0}
+end
